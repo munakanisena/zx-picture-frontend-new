2fca65d896c7049aa30f962c4c34e2c6be59f56c
munakanisena <1271626721@qq.com>
2025-07-22 18:39:53 +0800

    优化

--- src/ws/pictureEditWebSocket.ts
+++ src/ws/pictureEditWebSocket.ts
@@ -3,6 +3,8 @@
 // 2.WebSocket 各个事件的处理函数，比如连接成功和连接关闭事件，跟后端对应
 // 3.向 WebSocket 服务端发送消息的函数等
 
+const WS_BASE_URL = import.meta.env.VITE_WS_BASE_API_URL
+
 export default class PictureEditWebSocket {
   private pictureId: number
   private socket: WebSocket | null
@@ -14,17 +16,11 @@ export default class PictureEditWebSocket {
     this.eventHandlers = {} // 自定义事件处理器
   }
 
-
-
-
   /**
    * 初始化 WebSocket 连接
    */
   connect() {
-
-    const DEV_BASE_URL = "ws://localhost:8123";
-    const PROD_BASE_URL = "ws://picture.katomegumi.site";
-    const url = `${PROD_BASE_URL}/api/ws/picture/edit?pictureId=${this.pictureId}`
+    const url = `${WS_BASE_URL}/api/ws/picture/edit?pictureId=${this.pictureId}`
     this.socket = new WebSocket(url)
 
     // 设置携带 cookie
